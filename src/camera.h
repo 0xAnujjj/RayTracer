9b5f9cc3312446a18ecab20146bf70117ae822ac
Anuj <bhattanuj299@gmail.com>
2026-01-25 22:33:36 +0545

    Implemented diffuse reflection and ray bounce depth
    Added diffuse reflection by sampling random directions on the hemisphere in ray_color. Introduced a max_depth parameter to limit recursive ray bounces. Extended vec3 with random vector generation and hemisphere sampling utilities. Added firstDiffuseSphere.png as output.

--- src/camera.h
+++ src/camera.h
@@ -9,6 +9,7 @@ class camera {
     double aspect_ratio = 1.0; // ratio of width over height
     int image_width = 100; // rendered img. width in pixel count
     int samples_per_pixel = 10; // count of random samples for each pixel
+    int max_depth = 50; // maximum ray bounce depth
     
 void render(const hittable& world){
         initialize();
@@ -21,7 +22,7 @@ void render(const hittable& world){
            color pixel_color(0,0,0);
            for (int sample = 0; sample < samples_per_pixel; sample++) {
                 ray r = get_ray(i,j);
-                pixel_color += ray_color(r, world);
+                 pixel_color += ray_color(r, world, max_depth);
 
            }
             write_color(cout, pixel_samples_scale * pixel_color);
@@ -88,11 +89,15 @@ void initialize() {
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) const {
+        if (depth <= 0) {
+            return color(0,0,0);
+        }
         hit_record rec;
        
         if (world.hit(r, interval(0, infinity), rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+            vec3 direction = random_on_hemisphere(rec.normal);
+        return 0.5 * ray_color(ray(rec.p, direction), world, depth - 1);
         }
        
         vec3 unit_direction  = unit_vector(r.direction());
